802f524249bc512e484801a5f1ec6fb7aae46de9
ulyn <407620185@qq.com>
2014-09-11 22:36:47 +0800

    examples

--- examples/testType.js
+++ examples/testType.js
@@ -1,35 +1,15 @@
 var eos = require("node-eos");
-var RequestProtocol = eos.RequestProtocol;
-var params = eos.params;
 
-var appId = "test";
-var serviceId = "testType";
-var serviceVersion = "1.3";
-function Service(rpcContext){
-    this.rpcContext = rpcContext || new params.RpcContext();
+function TestType(){
+    eos.Service.call(this);
+    this.appId = "test";
+    this.serviceId = "testType";
+    this.serviceVersion = "1.3";
 }
-Service.prototype.testMap = function(map,str,successFunc,errorFunc){
-    var req = this._createReqPro(map,str,"testMap");
+eos.util.inherits(TestType,eos.Service);
+
+TestType.prototype.testMap = function(map,str,successFunc,errorFunc){
+    var req = this._createReqPro("testMap",map,str);
     eos.call(req,successFunc,errorFunc);
 }
-create
-Service.prototype._createReqPro = function(){
-    var method = arguments.pop();
-    var req = new RequestProtocol({
-        appId:appId,
-        serviceId:serviceId,
-        serviceVersion:serviceVersion,
-        mock:"",
-        debugServerIp:"",
-        rpcContext:this.rpcContext,
-        rpcInvocation:new params.RpcInvocation({
-            "@type":"com.sunsharing.eos.common.rpc.impl.RpcInvocation",
-            methodName:method,
-            parameterTypes:null,
-            arguments:arguments,
-            mock:""
-        })
-    });
-    return req;
-}
-module.exports = Service;
+module.exports = TestType;
