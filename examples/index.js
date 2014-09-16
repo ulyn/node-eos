31db2d87901a0f7aa5f78edd9cecff6869ad9fba
ulyn <407620185@qq.com>
2014-09-16 17:29:02 +0800

    fix bugs and impl mock config

--- examples/index.js
+++ examples/index.js
@@ -2,6 +2,6 @@ module.exports = function(eos){
     eos = eos || require("node-eos");
     return {
         eos:eos,
-        TestType:require("./TestType")(eos)
+        testType:require("./testType")(eos)
     }
 }
--- examples/testType.js
+++ examples/testType.js
@@ -1,15 +1,15 @@
 module.exports = function(eos){
-    function TestType(){
+    function testType(){
         eos.Service.call(this);
         this.appId = "test";
         this.serviceId = "testType";
         this.serviceVersion = "1.3";
     }
-    eos.util.inherits(TestType,eos.Service);
+    eos.util.inherits(testType,eos.Service);
 
-    TestType.prototype.testMap = function(map,str,successFunc,errorFunc){
+    testType.prototype.testMap = function(map,str,successFunc,errorFunc){
         var req = this._createReqPro("testMap",map,str);
         eos.call(req,successFunc,errorFunc);
     }
-    return TestType;
+    return testType;
 }
